ced8d499709bfbf3a1b39d609772e5687ca9b6b6
Lucas Mello <54722857+mellolucas@users.noreply.github.com>
2026-05-22 19:05:18 -0300

    feat: add essential nvim opts and brewfile

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -1,5 +1,6 @@
 -- Load all user custom configuration modules
 require("config.options")
-require("config.keymaps")
 -- require("config.autocmds")
+-- require("config.plugins")
 -- require("config.lsp")
+-- require("config.keymaps")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,4 +1,33 @@
+-- Native settings only (editor defaults and colorscheme command at the end)
 local opt = vim.opt
 
--- theme
-vim.g.catppuccin_flavour = "macchiato"
+-- UI
+opt.number = true -- Line numbers
+opt.relativenumber = true -- Relative line numbers
+opt.cursorline = true -- Highlight current line
+opt.wrap = false -- Don't wrap lines
+opt.scrolloff = 10 -- Keep 10 lines above/below cursor
+opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+opt.termguicolors = true -- Enable 24-bit colors
+opt.signcolumn = "yes" -- Always show sign column
+opt.showmatch = true -- Highlight matching brackets
+opt.matchtime = 2 -- How long to show matching bracket
+opt.cmdheight = 1 -- Command line height
+opt.showmode = false -- Don't show mode in command line
+
+-- Indentation
+opt.tabstop = 2 -- Tab width
+opt.shiftwidth = 2 -- Indent width
+opt.softtabstop = 2 -- Soft tab stop
+opt.expandtab = true -- Use spaces instead of tabs
+opt.smartindent = true -- Smart auto-indenting
+opt.autoindent = true -- Copy indent from current line
+
+-- Search settings
+opt.ignorecase = true -- Case insensitive search
+opt.smartcase = true -- Case sensitive if uppercase in search
+opt.hlsearch = false -- Don't highlight search results
+opt.incsearch = true -- Show matches as you type
+
+-- Set native colorscheme
+vim.cmd.colorscheme "catppuccin"
