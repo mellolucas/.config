d9cbb9e20776fd262658ffe67bbae05c2f1bd760
Lucas Mello <54722857+mellolucas@users.noreply.github.com>
2026-06-01 16:29:57 -0300

    chore(nvim): add essential keymaps and fix typo

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,1 +1,14 @@
+-- Minimal native keymaps
 local map = vim.keymap.set
+
+map("n", "<leader>e", vim.cmd.Explore, { desc = "Explore files" })
+
+map("n", "<leader>f", function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "Format buffer" })
+
+map("n", "<leader>q", vim.diagnostic.setqflist, { desc = "Diagnostics to quickfix" })
+
+nap("n", "<leader>h", "<cmd>nohlsearch<CR>", { desc = "Clear search highlight" })
+
+map("t", "<Esc><Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
